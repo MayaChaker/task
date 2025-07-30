d95d5846afb906451b59343c76e65719452ac605
Maya <maa232lb@aou.edu.lb>
2025-07-30 17:21:55 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/task/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+});

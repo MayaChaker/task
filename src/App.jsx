f013f2f84557b6392af293b5e78be84b66cf4756
Maya <maa232lb@aou.edu.lb>
2026-01-13 19:13:13 +0200

    Refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import react from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Home from "./components/Home";
+import Home from "./components/HomePage/Home";
 
 function App() {
   return (
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./style.css";
+import "../styles/style.css";
 import { Link } from "react-router-dom";
 
 function Login() {
